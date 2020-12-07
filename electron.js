4ea4aa976ef0446eac16760b9fd81f14669089ed
C1279225 Wallas Ferreira de Morais <c1279225@interno.bb.com.br>
2020-12-06 21:56:48 -0300

    some try

---
+++ electron.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    show: false,
+    frame: false,
+  });
+  const startURL = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.maximize();
+    mainWindow.show();
+  });
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+app.on('ready', createWindow);
